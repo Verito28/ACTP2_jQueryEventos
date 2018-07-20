0ab97577b4cbc0790afd4b93ae9c0cd5207f8a08
Veronica Macarena Muñoz Guajardo <vmunozg@novalte.corp>
2018-07-19 20:48:32 -0400

    ejercicio 5

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,15 +6,27 @@ $(document).ready(function(){
     });
 
       $(".card__like").click(function(event){
-      	event.stopPropagation();
-        $(".card__like--red").toggle();
+      	 $(this).toggleClass('card__like--red');
+
+        event.stopPropagation();
+      	event.preventDefault();
     });
 
       $(".card__follow-btn").click(function(event){
-      	event.stopPropagation();
-      	
+      	      	
         $(this).toggleClass('card__follow-btn--following');
+        event.stopPropagation();
     });
+
+
+        $(".create__select").change(function(event){
+        
+        $(".create__image img").attr("src","assets/images/squared/" + $(this).val());
+      	
+      
+        event.stopPropagation();
+    });
+
 
 
 
